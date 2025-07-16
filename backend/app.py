aac12d9b83a8b3a21afb6bf22fb33fa03e5be8a3
Shree Krishna Devkota <shreekrishna1514@gmail.com>
2025-07-16 11:40:20 +0545

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -1,5 +1,11 @@
 from flask import Flask, request, jsonify
 import numpy as np
+import os
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
+
 
 app = Flask(__name__)
 
